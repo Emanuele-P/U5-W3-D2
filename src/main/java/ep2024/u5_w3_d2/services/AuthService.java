07bb8906b4aa00d76570674572fef8effc2eb8c7
Emanuele-P <161461547+Emanuele-P@users.noreply.github.com>
2024-07-02 16:46:54 +0200

    implements bcrypt

--- src/main/java/ep2024/u5_w3_d2/services/AuthService.java
+++ src/main/java/ep2024/u5_w3_d2/services/AuthService.java
@@ -5,6 +5,7 @@ import ep2024.u5_w3_d2.exceptions.UnauthorizedException;
 import ep2024.u5_w3_d2.payloads.EmployeeLoginDTO;
 import ep2024.u5_w3_d2.security.JWTTools;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,10 +16,13 @@ public class AuthService {
     @Autowired
     private JWTTools jwtTools;
 
+    @Autowired
+    private PasswordEncoder bcrypt;
+
     public String authenticateUserAndGenerateToken(EmployeeLoginDTO payload) {
 
         Employee employee = this.employeesService.findByEmail(payload.email());
-        if (employee.getPassword().equals(payload.password())) {
+        if (bcrypt.matches(payload.password(), employee.getPassword())) {
             return jwtTools.createToken(employee);
         } else {
             throw new UnauthorizedException("Your credentials are incorrect!");
